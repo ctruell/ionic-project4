da2625b323fdfd75f7e01736539e8b599d67a519
ctruell <cet2576@rit.edu>
2018-12-14 22:30:40 -0500

    commit

---
+++ src/app/about/about.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AppVersion } from '@ionic-native/app-version;';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: 'about.page.html',
+  styleUrls: ['about.page.scss']
+})
+export class AboutPage {
+  version() {
+    var appVersion = "1.0.0";
+    document.getElementById("ver").innerHTML = appVersion;
+
+    /*cordova.getAppVersion.getVersionNumber(function (version) {
+      alert(version);
+    });*/
+  }
+}
